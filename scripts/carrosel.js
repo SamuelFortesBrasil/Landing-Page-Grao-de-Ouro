5b9c8502bae9613991943a58d4920bf6d6c9499f
Samuel Fortes Brasil <samueldbz441@gmail.com>
2026-01-30 14:19:34 -0300

    Projeto Finalizado

--- scripts/carrosel.js
+++ scripts/carrosel.js
@@ -3,7 +3,7 @@ const clientes = document.querySelectorAll('.cliente')
 
 let index = 0
 
-
+//Função normal, não sei porque, mais não quis usar um arrow function
 function carrosel(){
     index++
     if(index >= clientes.length){
@@ -12,5 +12,6 @@ function carrosel(){
     container.style.transform = `translateX(${ -index * 100}%)`
 }
 
+//Carrosel de Comentários de clientes
 setInterval(carrosel,3000)
 
